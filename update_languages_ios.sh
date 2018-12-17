4771218232afb6f7f9a4f462a1eccc2eed6401c9
Arturo Filastò <arturo@filasto.net>
2018-12-17 12:38:06 +0100

    Fixes to update_languages_*.sh

--- update_languages_ios.sh
+++ update_languages_ios.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-source supported_languages
 set -e
 
-PROJDIR="../probe-ios/"
-cd $PROJDIR;
-TOPDIR=$(cd $PROJDIR && pwd -P)
+PROJDIR="../probe-ios"
+TOPDIR=$(cd $(dirname $0)/.. && pwd -P)
 
-if [ ! -d ooniprobe/ ];then
+if [ ! -d ${PROJDIR}/ooniprobe/ ];then
     echo "check your PROJDIR variable, it should be the directory where you cloned https://github.com/ooni/probe-ios"
     exit 1
 fi
 
-cd ../translations
+source supported_languages_mobile.sh
+
 ./update-translations.sh
 for language in "${SUPPORTED_LANGUAGES[@]}";do
     lang=$(basename ${language} | sed 's/zh_CN/zh-Hans/' | sed 's/zh_TW/zh-Hant/')
-    dst_path="${TOPDIR}/ooniprobe/${lang}.lproj/"
+    dst_path="${PROJDIR}/ooniprobe/${lang}.lproj/"
     if [ ! -d "$dst_path" ];then
         echo "error: $dst_path doesn't exist, remember to add the language to the project and rerun the script"
         else
@@ -23,4 +22,4 @@ for language in "${SUPPORTED_LANGUAGES[@]}";do
     fi
 done
 echo "Translations have been updated"
-echo "Remember to commit the changes made to ooni/translations!"
+echo "Remember to commit the changes!"
