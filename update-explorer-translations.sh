bed9b3ecfd21a4b93272eb93adb3ac1c6018e3a1
Maja Komel <maja.komel@gmail.com>
2023-06-19 10:49:46 +0200

    Update explorer strings

--- update-explorer-translations.sh
+++ update-explorer-translations.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
 
-MINIMUM_TRANSLATION_NEEDED=${1:-100}
+MINIMUM_TRANSLATION_NEEDED=${1:-80}
 
 echo "Fetching translations for languages with at least ${MINIMUM_TRANSLATION_NEEDED}% completion..."
 
 tx pull -a \
   -r ooni-explorer.website \
-  --minimum-perc=${MINIMUM_TRANSLATION_NEEDED} \
-  --no-interactive
+  --minimum-perc=${MINIMUM_TRANSLATION_NEEDED}
