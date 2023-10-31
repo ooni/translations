a8bdb78b036cfe862d42807f1d1be74fdb85de87
Norbel Ambanumben <aanorbel@gmail.com>
2023-10-31 20:59:14 +0100

    Updated languages for mobile

--- supported_languages_mobile.sh
+++ supported_languages_mobile.sh
@@ -1,1 +1,1 @@
-SUPPORTED_LANGUAGES=(ar ca de el es fa fr hi id is it nl pt_BR ro ru sw sk sq th tr zh_CN zh_TW)
+SUPPORTED_LANGUAGES=(ar ca de el es fa fr hi id is it nl pt_BR ro ru sw sk sq th tr zh_CN zh_TW my vi)
